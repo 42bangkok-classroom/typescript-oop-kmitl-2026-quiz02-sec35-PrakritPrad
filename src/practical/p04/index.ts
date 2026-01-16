554693b62d9c88e50dff025061401951ce751202
Prakrit khwanphatthalung <poom66310@gmail.com>
2026-01-16 10:51:37 +0700

    13

--- src/practical/p04/index.ts
+++ src/practical/p04/index.ts
@@ -1,1 +1,25 @@
-export function countCommentsByPost() {}
+import axios from "axios";
+
+interface Comment {
+  id: number;
+  postId: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+export async function countCommentsByPost(): Promise<Record<number, number>> {
+  try {
+    const res = await axios.get<Comment[]>(
+      "https://jsonplaceholder.typicode.com/comments"
+    );
+
+    return res.data.reduce<Record<number, number>>((acc, comment) => {
+      acc[comment.postId] = (acc[comment.postId] ?? 0) + 1;
+      return acc;
+    }, {});
+  } catch {
+    return {};
+  }
+}
+
