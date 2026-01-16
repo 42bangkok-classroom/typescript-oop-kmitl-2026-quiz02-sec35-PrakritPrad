8f90067769ef23734e925f64c9021e8265309bc9
Prakrit khwanphatthalung <poom66310@gmail.com>
2026-01-16 10:59:52 +0700

    lastone

--- src/practical/p05/index.ts
+++ src/practical/p05/index.ts
@@ -1,2 +1,29 @@
-export function safeFetchComment() {}
+import axios from "axios";
+
+interface Comment {
+  id: number;
+  postId: number;
+  name: string;
+  email: string;
+  body: string;
+}
+
+export async function safeFetchComment(
+  commentId: number
+): Promise<{ id: number; body: string } | null> {
+  try {
+    const res = await axios.get<Comment>(
+      `https://jsonplaceholder.typicode.com/comments/${commentId}`
+    );
+
+    return {
+      id: res.data.id,
+      body: res.data.body
+    };
+  } catch {
+    return null;
+  }
+}
+
+
 
