a5506306a84ce176e7b2ae8db0bee21c4a7c77a1
Prakrit khwanphatthalung <poom66310@gmail.com>
2026-01-16 10:13:32 +0700

    8

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,1 +1,29 @@
-export function getPostsByUser() {}
+import axios from "axios"
+import { title } from "node:process";
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export async function getEdgePosts(userId: number): Promise<{ id: number, title: string }[]> {
+  try {
+
+    const res = await axios.get<Post[]>(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+
+    return res.data
+      .filter(post => post.userId === userId)
+      .map(post => ({
+        id: post.id,
+        title: post.title
+      }));
+  }
+  catch (err) {
+    throw err
+  }
+}
+
